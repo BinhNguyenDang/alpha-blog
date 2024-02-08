7fcf97be2187cd984203a356cfe09284b13a76aa
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-02-07 22:41:18 -0500

    route + articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
 
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
